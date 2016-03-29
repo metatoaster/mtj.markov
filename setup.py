bb4e9f34a3da99558586f80d8f1f9c9731166f48
Tommy Yu <y@metatoaster.com>
2016-03-29 22:49:28 +1300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.0'
+
+setup(name='mtj.markov',
+      version=version,
+      description="Markov text generator",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.rst")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Tommy Yu',
+      author_email='y@metatoaster.com',
+      url='https://github.com/metatoaster/mtj.markov',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['mtj'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
